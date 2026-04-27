129725899f0c7c0564d48a584dc5c0a9afd88114
0xdorin <wjs505@gmail.com>
2026-04-27 16:48:58 +0900

    [chore] scaffold extension project

---
+++ src/panel/App.tsx
@@ -0,0 +1,12 @@
+export function App() {
+  return (
+    <main className="app-shell">
+      <header className="app-header">
+        <div>
+          <h1>Notion HWPX</h1>
+          <p>Upload a template and a Notion export to generate an HWPX file.</p>
+        </div>
+      </header>
+    </main>
+  );
+}
